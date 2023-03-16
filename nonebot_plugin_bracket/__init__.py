fb895e04dc8d9e2527b03c339abd386fa9a3ab6e
meetwq <meetwq@gmail.com>
2023-03-16 16:45:05 +0800

    fix

--- nonebot_plugin_bracket/__init__.py
+++ nonebot_plugin_bracket/__init__.py
@@ -1,12 +1,12 @@
 from nonebot import on_message
-from nonebot.typing import T_State
 from nonebot.params import EventPlainText
 from nonebot.plugin import PluginMetadata
+from nonebot.typing import T_State
 
 __plugin_meta__ = PluginMetadata(
     name="括号补全",
     description="补全消息中的括号，治愈强迫症",
-    usage="",
+    usage="发送带括号的消息即可",
 )
 
 bracket_pairs = {
@@ -15,6 +15,9 @@ bracket_pairs = {
     "[": "]",
     "【": "】",
     "{": "}",
+    "｛": "｝",
+    "<": ">",
+    "《": "》",
 }
 
 
@@ -24,12 +27,13 @@ async def check_brackets(state: T_State, text: str = EventPlainText()) -> bool:
         for open_bracket, close_bracket in bracket_pairs.items():
             if char == open_bracket:
                 brackets.append(close_bracket)
+                break
             elif char == close_bracket:
                 if not brackets or brackets.pop() != close_bracket:
                     return False
     if not brackets:
         return False
-    state["brackets"] = "".join(brackets)
+    state["brackets"] = "".join(reversed(brackets))
     return True
 
 
